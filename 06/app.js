bd700d6559fbf001fd79b5dcb7ae87fba7830f4c
kubala1210 <jakubgromala@wp.pl>
2025-07-16 18:46:20 +0100

    update06

--- 06/app.js
+++ 06/app.js
@@ -1,17 +1,23 @@
 console.log('DOM');
 
 const buttonList = document.querySelectorAll('button');
-let counter = 0;
 
 const afterClick = function (event) {
     event.target.innerText = 'clicked';
-    ++counter;
-    if (counter === 1) {
-        event.target.removeEventListener('click', afterClick)
-    }
+    event.target.removeEventListener('click', afterClick)
     console.log('clicked');
 };
 
 buttonList.forEach(function (button) {
     button.addEventListener('click', afterClick)
 });
+
+// counter = counter + 1
+
+// const bodyEl = document.querySelector('body');
+
+// console.log(bodyEl)
+
+// bodyEl.addEventListener('click', function (event) {
+//     console.log(event.target, event.currentTarget)
+// })
